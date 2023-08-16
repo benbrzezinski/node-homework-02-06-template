d3850d17572aea59c2a7ebd6974c6f1cc83e02a7
Beniamin Brzeziński <benek9311@wp.pl>
2023-08-16 19:26:05 +0200

    Improved auth middleware

--- server/routes/api/authorization.js
+++ server/routes/api/authorization.js
@@ -2,7 +2,11 @@ import passport from "passport";
 
 const auth = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (err, user) => {
-    if (err || !user) {
+    if (
+      err ||
+      !user ||
+      req.headers.authorization.split(" ").at(1) !== user.token
+    ) {
       return res.status(401).json({
         status: 401,
         statusText: "Unauthorized",
